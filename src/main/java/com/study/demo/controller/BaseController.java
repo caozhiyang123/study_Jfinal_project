28a041870be89a3311f5ac2e79612ac407ce58bd
caozhiyang123 <1250233433@qq.com>
2019-03-26 14:22:29 +0800

    refactor QrCode

--- src/main/java/com/study/demo/controller/BaseController.java
+++ src/main/java/com/study/demo/controller/BaseController.java
@@ -1,6 +1,7 @@
 package com.study.demo.controller;
 
 import com.jfinal.core.Controller;
+import com.study.demo.render.PhotoQrCode;
 
 public class BaseController extends Controller
 {
@@ -15,7 +16,8 @@ public class BaseController extends Controller
     }
     
     public void QrCode(){
-        renderQrCode("content", 100, 100);
+        render(new PhotoQrCode("http://www.baifenjy.com", 150, 150
+                ,"http://www.gtdblog.com/wp/wp-content/uploads/2017/09/meyou4-1.png",0xff9966));
     }
     
 }
